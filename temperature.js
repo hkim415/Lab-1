287e2533fad719435a3ca8adbed7c6dde3cde26d
Hongsoon Kim <khsun6226@naver.com>
2020-09-19 18:51:18 -0700

    finish the function that changes temperature.

--- temperature.js
+++ temperature.js
@@ -1,8 +1,28 @@
-let c = 0;
 
-let fToC = (f) => {
-    c = (f - 32) * 5 / 9;
-    console.log("When it's " + f + " degrees Fahrenheit, it is " + c + " degrees Celcius.");
+// A function that converts fahrenheit to celcius.
+const fToC = (f) => {
+    //formula for fahrenheit to celcius
+    let c = (f - 32) * 5 / 9;
+
+    //conditional display depending on the value of f(farenheit)
+    if(!f) {
+        console.log("Please enter the temperature.");
+    } else if(c.toFixed(1)-Math.floor(c) === 0){
+        console.log("When it's " + f + " degrees Fahrenheit, it is " + Math.floor(c) + " degrees Celcius.");
+    } else {
+        console.log("When it's " + f + " degrees Fahrenheit, it is " + c.toFixed(1) + " degrees Celcius.");
+    }
 };
 
-fToC(32);
+const toF = (c, k) => {
+    //formula for celcius to fahrenheit
+    let cf = (c * 9/5) + 32;
+    //formula for kelvin to fahrenheit
+    let kf = (k - 273.15) * 9/5 + 32;
+
+    console.log("When it's " + c + " degrees Celcius, it is " + cf.toFixed(1) + " degrees Fahrenheit.");
+    console.log("When it's " + k + " degrees Kelvin, it is " + kf.toFixed(1) + " degrees Fahrenheit.");
+}
+
+fToC(68);
+toF(0, 0);
